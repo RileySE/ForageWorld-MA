955e64becfd7a0720330cd8bf6c316cdffab099d
StyraxW <xicun0831@gmail.com>
2025-12-28 18:26:23 -0500

    Update craftax_state.py
    define player attribute of subclasses: player_sc

--- craftax/craftax_coop/craftax_state.py
+++ craftax/craftax_coop/craftax_state.py
@@ -71,7 +71,7 @@ class EnvState:
     player_hunger: jnp.ndarray
     player_thirst: jnp.ndarray
     player_fatigue: jnp.ndarray
-    player_recover_mana: jnp.ndarray
+    player_recover_mana: jnp.ndarray 
 
     # Attributes
     player_xp: jnp.ndarray
@@ -79,6 +79,7 @@ class EnvState:
     player_strength: jnp.ndarray
     player_intelligence: jnp.ndarray
     player_specialization: jnp.ndarray
+    player_sc: jnp.ndarray # subclasses
 
     # Request Info
     request_duration: jnp.ndarray
