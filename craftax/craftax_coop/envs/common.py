69116ef72bb5c852dfe7c4cf12099d5c3d71362e
StyraxW <xicun0831@gmail.com>
2026-01-07 16:39:33 -0500

    Update common.py
    add the logging of interactions to info

--- craftax/craftax_coop/envs/common.py
+++ craftax/craftax_coop/envs/common.py
@@ -5,8 +5,12 @@ from craftax_coop.constants import *
 
 def compute_score(state: EnvState, done: bool, static_params: StaticEnvParams):
     achievements = state.achievements * done * 100.0
+    interactions = state.interactions * done * 100.0
     info = {}
     for achievement in Achievement:
         achievement_name = f"Achievements/{achievement.name.lower()}"
         info[achievement_name] = achievements[:, achievement.value]
+    for i in Interaction:
+        interaction_name = f"Interactions/{interaction.name.lower()}"
+        info[interaction_name] = interactions[:, interaction.value]
     return info
