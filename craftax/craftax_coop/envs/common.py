919bc8796b39df02e58fbd57345bfb2ff341be2d
StyraxW <xicun0831@gmail.com>
2026-01-07 18:14:14 -0500

    Update common.py

--- craftax/craftax_coop/envs/common.py
+++ craftax/craftax_coop/envs/common.py
@@ -5,12 +5,18 @@ from craftax_coop.constants import *
 
 def compute_score(state: EnvState, done: bool, static_params: StaticEnvParams):
     achievements = state.achievements * done * 100.0
-    interactions = state.interactions * done * 100.0
     info = {}
     for achievement in Achievement:
         achievement_name = f"Achievements/{achievement.name.lower()}"
         info[achievement_name] = achievements[:, achievement.value]
-    for i in Interaction:
+
+    return info
+    
+def log_interactions(state: EnvState, done: bool, static_params: StaticEnvParams, actor, receiver):
+    info = {}
+    interactions = state.interactions * done * 100.0
+    for interaction in Interaction:
+        name = interaction.name.lower()
         interaction_name = f"Interactions/{interaction.name.lower()}"
-        info[interaction_name] = interactions[:, interaction.value]
+        info[interaction_name] = interactions[:, interaction.value]      
     return info
